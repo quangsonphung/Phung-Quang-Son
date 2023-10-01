9955414dbb677a1aec063116f4e9c04e94dc45c8
Phung Son <phungquangson17@gmail.com>
2023-10-01 11:30:48 +0700

    buoi2

---
+++ app/Route.php
@@ -0,0 +1,38 @@
+<?php
+class Route
+{
+    public static function route_site()
+    {
+        $pathView = "views/frontend/";
+        if (!isset($_REQUEST["option"])) {
+            $pathView .= "home.php";
+        } else {
+            $pathView .= $_REQUEST["option"];
+            if (!isset($_REQUEST["slug"])) {
+                $pathView .= "-detail.php";
+            } else {
+                if (!isset($_REQUEST["cat"])) {
+                    $pathView .= "-category.php";
+                } else {
+                    $pathView .= ".php";
+                }
+            }
+        }
+        require_once $pathView;
+    }
+    public static function route_admin()
+    {
+        $pathView = "../views/backend/";
+        if (!isset($_REQUEST['option'])) {
+            $pathView .= "dashboard/index.php";
+        } else {
+            $pathView .= $_REQUEST['option'] . "/";
+            if (!isset($_REQUEST['cat'])) {
+                $pathView .= $_REQUEST['cat'] . ".php";
+            } else {
+                $pathView .= "index.php";
+            }
+        }
+        require_once $pathView;
+    }
+}
